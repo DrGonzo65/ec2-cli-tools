e1c673462f9b869586f309da3d4a3431eaec295c
Nick Shortway <nick@instagram.com>
2013-10-24 15:03:23 -0700

    support overlay IP

--- setup.py
+++ setup.py
@@ -86,7 +86,7 @@ from setuptools import setup
 
 setup(
     name = "ec2-cli-tools",
-    version = "1.5",
+    version = "1.5.1",
     author = "Shayne Sweeney & Tyler Smalley & Mike Krieger & Nick Shortway",
     author_email = "team@flippath.com",
     description = "Helpful CLI utilities for querying and connecting to EC2 instances",
